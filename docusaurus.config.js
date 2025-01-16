c29f94cc361697c9d54c6c4a1d38a43c65733dae
sofia <sofia@dtonic.io>
2025-01-16 17:31:40 +0900

    hotfix2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,8 @@ const config = {
 	url: "https://your-docusaurus-site.example.com",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/build/",
+	// baseUrl: "/build/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
